db9f1be8fbe6b05e76ab289c84853b62b1565f0d
albertchitta <albert.w.chittaphong@gmail.com>
2021-09-15 18:04:28 -0500

    Added getOrders and getSingleOrder functions

---
+++ src/scripts/helpers/data/orders.js
@@ -0,0 +1,21 @@
+// API CALLS FOR ORDERS
+import axios from 'axios';
+import firebaseConfig from '../../../api/apiKeys';
+
+const dbUrl = firebaseConfig.databaseURL;
+
+// GET ORDERS
+const getOrders = (userId) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/orders.json?orderBy="uid"&equalTo="${userId}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+// GET SINGLE BOOK
+const getSingleOrder = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/orders/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+export { getOrders, getSingleOrder };
