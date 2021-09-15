e1af881bad815424ade9ddfa77fba5b27b128017
pmpurcell93 <maddenpurcell@gmail.com>
2021-09-15 18:24:29 -0500

    Created buildOrderForm

--- src/scripts/helpers/buildOrderForm.js
+++ src/scripts/helpers/buildOrderForm.js
@@ -6,23 +6,19 @@ const buildOrderForm = (obj = { }) => {
     <input type="text" class="form-control" id="orderName" value="${obj.customername}>
   </div>
   <div class="mb-3">
-  <label for="customerPhone" class="form-label">Customer Phone</label>
-  <input type="text" class="form-control" id="customerPhone" value="${obj.customerphone}>
+    <label for="customerPhone" class="form-label">Customer Phone</label>
+    <input type="text" class="form-control" id="customerPhone" value="${obj.customerphone}>
   </div>
   <div class="mb-3">
-  <label for="customerEmail" class="form-label">Customer Email</label>
-  <input type="text" class="form-control" id="customerEmail" value="${obj.customeremail}>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="text" class="form-control" id="exampleInputPassword1">
+    <label for="customerEmail" class="form-label">Customer Email</label>
+    <input type="text" class="form-control" id="customerEmail" value="${obj.customeremail}>
   </div>
   <select class="form-select" aria-label="Default select example" value="${obj.ordertype}>
-  <option selected>Order Type</option>
-  <option value="1">Phone</option>
-  <option value="2">In-Person</option>
+    <option selected>Order Type</option>
+    <option value="1">Phone</option>
+    <option value="2">In-Person</option>
   </select>
-  <button type="submit" id="${obj.firebaseKey ? } class="btn btn-primary">Submit</button>
+  <button type="submit" id="${obj.firebaseKey ? `update-order--${obj.firebaseKey}` : 'submit-order'} class="btn btn-primary">Create/Edit Order</button>
 </form>
   `
 }
