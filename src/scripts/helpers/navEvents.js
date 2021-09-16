e1807767ddb7dcc60033cdfc8dc2bdceeb50b5ad
Gabriel Smith <gabrielsmith@Gabriels-MacBook-Pro-2.local>
2021-09-15 19:52:12 -0500

    view orders event

--- src/scripts/helpers/navEvents.js
+++ src/scripts/helpers/navEvents.js
@@ -4,5 +4,10 @@ const navEvents = () => {
   document.querySelector('#create-order').addEventListener('click', () => {
     buildOrderForm();
   });
+
+  document.querySelector('#view-orders').addEventListener('click', () => {
+    console.warn('View Orders');
+    // getOrders(uid).then(showOrders);
+  });
 };
 export default navEvents;
