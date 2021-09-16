509ce3ea9b99afa64b63784df8c2e693d80f2a3c
Halie Dyer <Halie.L.Dyer@gmail.com>
2021-09-15 19:11:05 -0500

    showorders

--- src/scripts/startApp.js
+++ src/scripts/startApp.js
@@ -1,12 +1,15 @@
 import homeLoggedIn from './components/homeLoggedIn';
 import navBar from './components/navBar';
+import showOrders from './components/showOrders';
 import domBuilder from './helpers/domBuilder';
+import { getOrders } from './helpers/data/orders';
 
-const startApp = () => {
+const startApp = (user) => {
   console.warn('App started!');
   domBuilder();
   navBar();
   homeLoggedIn();
+  getOrders(user.uid).then(showOrders);
 };
 
 export default startApp;
