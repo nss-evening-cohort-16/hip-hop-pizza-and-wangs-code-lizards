11f0fac4c1c8c591f19b213b6c3d009b79f82fce
albertchitta <albert.w.chittaphong@gmail.com>
2021-09-15 19:32:01 -0500

    Added click events for viewing orders and viewing revenue

--- src/scripts/events/domEvents.js
+++ src/scripts/events/domEvents.js
@@ -1,10 +1,22 @@
 import buildOrderForm from '../helpers/buildOrderForm';
+// import { getOrders } from '../helpers/data/ordersData';
 
 const domEvents = (uid) => {
   document.querySelector('#mainContainer').addEventListener('click', (e) => {
     // CLICK EVENT FOR SHOWING FORM FOR CREATING AN ORDER
     if (e.target.id.includes('create-order')) {
       buildOrderForm(uid);
+    }
+
+    // CLICK EVENT FOR VIEWING ORDERS
+    if (e.target.id.includes('view-orders')) {
+      console.warn('View Orders');
+      // getOrders(uid).then(showOrders);
+    }
+
+    // CLICK EVENT FOR VIEWING REVENUE
+    if (e.target.id.includes('view-revenue')) {
+      console.warn('View Revenue');
     }
   });
 };
