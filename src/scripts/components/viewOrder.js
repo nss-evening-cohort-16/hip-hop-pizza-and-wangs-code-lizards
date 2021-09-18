23b1c3d6303497aa4c8ebaf381749f58717111cf
albertchitta <albert.w.chittaphong@gmail.com>
2021-09-18 12:41:43 -0500

    Added add-item-btn and payment-btn

--- src/scripts/components/viewOrder.js
+++ src/scripts/components/viewOrder.js
@@ -6,7 +6,7 @@ const viewOrder = (obj) => {
   obj.items.forEach((item) => {
     document.querySelector('#detailsContainer').innerHTML += `
       <div class="card">
-        <div class="card-body" style="height: 30px;">
+        <div class="card-body" style="height: 124px;">
           <h5 class="card-title">${item.itemname}</h5>
           <h5>PRICE: $${item.itemprice}</h5>
         </div>
@@ -15,6 +15,8 @@ const viewOrder = (obj) => {
           <a id="delete-order--${item.firebaseKey}" href="#" class="card-link">Delete Item</a>
         </div>
       </div>
+      <button type="button" class="btn btn-success" id="add-item-btn">Add Item</button>
+      <button type="button" class="btn btn-primary" id="payment-btn">Go To Payment</button>
     `;
   });
 };
