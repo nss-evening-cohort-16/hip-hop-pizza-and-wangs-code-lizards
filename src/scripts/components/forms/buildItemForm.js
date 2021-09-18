4f8b02444bbebf2b19b046620afccd76684439cb
pmpurcell93 <maddenpurcell@gmail.com>
2021-09-18 10:15:44 -0500

    Changed value from obj.name to obj.itemname to reflect data

--- src/scripts/components/forms/buildItemForm.js
+++ src/scripts/components/forms/buildItemForm.js
@@ -6,7 +6,7 @@ const buildItemForm = (obj = { }) => {
     <form id="item-form">
       <div class="mb-3">
         <label for="itemName" class="form-label">Item Name</label>
-        <input type="text" class="form-control" id="itemName" value="${obj.name || ''}">
+        <input type="text" class="form-control" id="itemName" value="${obj.itemname || ''}">
       </div>
       <div class="mb-3">
         <label for="itemPrice" class="form-label">Item Price</label>
