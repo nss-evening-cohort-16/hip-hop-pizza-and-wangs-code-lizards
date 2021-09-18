4158c33ca25697c4c5ee354b3d8b4d7a5a180a54
pmpurcell93 <maddenpurcell@gmail.com>
2021-09-18 12:25:05 -0500

    Create createOrder function

--- src/scripts/helpers/data/ordersData.js
+++ src/scripts/helpers/data/ordersData.js
@@ -22,13 +22,13 @@ const getSingleOrder = (firebaseKey) => new Promise((resolve, reject) => {
 const createOrder = (cardObj) => new Promise((resolve, reject) => {
   axios.post(`${dbUrl}/orders.json`, cardObj)
     .then((response) => {
-        const body = { firebaseKey: response.data.name };
-    axios.patch(`${dbUrl}/orders/${response.data.name}.json`, body)
-    .then(() => {
-      getOrders(cardObj.userId).then(resolve);
-    });
-  })
-  .catch(reject);
-})
+      const body = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/orders/${response.data.name}.json`, body)
+        .then(() => {
+          getOrders(cardObj.userId).then(resolve);
+        });
+    })
+    .catch(reject);
+});
 
 export { getOrders, getSingleOrder, createOrder };
