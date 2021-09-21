19f2a522a10fcda2240cf8c67b69c3dbc7172ddd
Gabriel Smith <gabrielsmith@Gabriels-MacBook-Pro-2.local>
2021-09-21 16:34:46 -0500

    items reducing

--- src/scripts/components/viewOrder.js
+++ src/scripts/components/viewOrder.js
@@ -2,12 +2,13 @@ import clearDom from '../helpers/clearDom';
 
 const viewOrder = (obj) => {
   clearDom();
+  const addedPrice = obj.items.reduce((total, item) => total + item.itemprice, 0);
   document.querySelector('#detailsContainer').innerHTML += `
   <div id="orderTotal">
-  <h5>Order Total: $${obj.items[0].itemprice}<h5>`;
+  <h5>Order Total: $${addedPrice}<h5>`;
 
   document.querySelector('#buttons-container').innerHTML = `
-  <button type="button" class="btn btn-success" id="add-item-btn--${obj.firebaseKey}">Add Item</button>
+  <button type="button" class="btn btn-success" id="add-item-btn">Add Item</button>
   <button type="button" class="btn btn-primary" id="payment-btn">Go To Payment</button>
   `;
 
--- src/scripts/helpers/data/mergedData.js
+++ src/scripts/helpers/data/mergedData.js
@@ -1,13 +1,15 @@
 import { getItems } from './itemsData';
-import { getOrderPrice, getSingleOrder } from './ordersData';
+import { getSingleOrder } from './ordersData';
 
 // VIEW ORDER DETAILS
 const viewOrderDetails = async (firebaseKey) => {
   const order = await getSingleOrder(firebaseKey);
   const items = await getItems(firebaseKey);
-  const price = await getOrderPrice(firebaseKey);
-  console.warn({ items, ...order, ...price });
-  return ({ items, ...order, ...price });
+  console.warn(order);
+  return ({
+    items,
+    ...order
+  });
 };
 
 export default viewOrderDetails;
