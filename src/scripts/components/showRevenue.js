f0ac5a08148228d80c0f44e104ce89079360c30e
Gabriel Smith <gabrielsmith@Gabriels-MacBook-Pro-2.local>
2021-09-25 09:19:35 -0500

    into one function

--- src/scripts/components/showRevenue.js
+++ src/scripts/components/showRevenue.js
@@ -3,14 +3,10 @@ import clearDom from '../helpers/clearDom';
 const viewRevenue = (obj) => {
   clearDom();
   const orderRevenue = obj.payments.reduce((total, payments) => total + payments.orderprice + payments.ordertip, 0);
+  const tipRevenue = obj.payments.reduce((total, payments) => total + payments.ordertip, 0);
   document.querySelector('#detailsContainer').innerHTML += `
     <div id="orderTotal">
-    <h1>Total Revenue: $${orderRevenue}<h1>`;
-
-  const tipRevenue = obj.payments.reduce((total, payments) => total + payments.ordertip, 0);
-  Math.round(tipRevenue);
-  document.querySelector('#detailsContainer').innerHTML += `
-    <div id="tipTotal">
+    <h1>Total Revenue: $${orderRevenue}<h1>
     <h1>Total Tips: $${tipRevenue}<h1>`;
 };
 export default viewRevenue;
